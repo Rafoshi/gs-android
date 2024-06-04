fed95ef69de6105da2160ca717d99616ef7b6a49
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-03 21:23:11 -0300

    First commit

---
+++ app/src/main/java/rafoshi/com/rafaelioshi_rm93102_modelo_recyclerview/MainActivity.kt
@@ -0,0 +1,19 @@
+package rafoshi.com.rafaelioshi_rm93102_modelo_recyclerview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import rafoshi.com.rafaelioshi_rm93102_modelo_recyclerview.ui.theme.RafaelIoshi_RM93102_modelo_recyclerViewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
